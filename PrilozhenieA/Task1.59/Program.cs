47ba5b63dd697edad7a34783900114ace518f930
PoleshchukKatya <161806444+PoleshchukKatya@users.noreply.github.com>
2024-03-03 15:47:22 +0300

    Add files via upload

---
+++ PrilozhenieA/Task1.59/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Task10;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число а:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int temp = a; 
+        a = b;       
+        b = temp;    
+
+        Console.WriteLine($"После обмена: a = {a}, b = {b}");
+        Console.ReadLine(); 
+    }
+}
---
+++ PrilozhenieA/Task1.60/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Task11;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число а:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Схема а):");
+        Console.WriteLine($"До обмена: a = {a}, b = {b}, c = {c}");
+        int temp = b; 
+        b = c;
+        c = a;
+        a = temp;
+        Console.WriteLine($"После обмена: a = {a}, b = {b}, c = {c}");
+
+        Console.WriteLine("\nСхема б):");
+        Console.WriteLine($"До обмена: a = {a}, b = {b}, c = {c}");
+        temp = b; 
+        b = a;
+        a = c;
+        c = temp;
+        Console.WriteLine($"После обмена: a = {a}, b = {b}, c = {c}");
+        Console.ReadLine(); 
+    }
+}
