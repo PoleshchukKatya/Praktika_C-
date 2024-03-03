ed9751dc499742e8e3453fc40e20949a0f1e3967
PoleshchukKatya <161806444+PoleshchukKatya@users.noreply.github.com>
2024-03-03 15:45:19 +0300

    Add files via upload

---
+++ PrilozhenieA/Task1.25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Task8;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину стороны квадрата:");
+        double sideLength = Convert.ToDouble(Console.ReadLine());
+
+        double perimeter = 4 * sideLength;
+
+        Console.WriteLine($"Периметр квадрата со стороной {sideLength} равен {perimeter}");
+        Console.ReadLine(); 
+    }
+}
---
+++ PrilozhenieA/Task1.26/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Task9;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите радиус окружности:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double diameter = 2 * radius;
+
+        Console.WriteLine($"Диаметр окружности с радиусом {radius} равен {diameter}");
+        Console.ReadLine();
+    }
+}
