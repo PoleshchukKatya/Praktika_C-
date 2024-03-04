4d6deb28cf46d1a61ab81583929011877963401f
PoleshchukKatya <161806444+PoleshchukKatya@users.noreply.github.com>
2024-03-04 23:50:18 +0300

    Add files via upload

---
+++ Task 3.3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Task3_3_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Трехзначные автоморфные числа:");
+
+        for (int i = 100; i < 1000; i++)
+        {
+            int square = i * i;
+            string squareStr = square.ToString();
+            string numberStr = i.ToString();
+
+            if (squareStr.EndsWith(numberStr))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
