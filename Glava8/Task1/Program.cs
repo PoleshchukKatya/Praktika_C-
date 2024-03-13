57c85625ff3cf9c380c28c470380e12cb8a5f937
Poleshchuk Katya <poleshchkat@gmail.com>
2024-03-13 22:47:44 +0300

    Добавляем Glava8

---
+++ Glava8/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Task1
+{
+    struct STUDENT
+    {
+        public string fullName;
+        public int groupNumber;
+        public int[] grades;
+
+        public STUDENT(string name, int group, int[] marks)
+        {
+            fullName = name;
+            groupNumber = group;
+            grades = marks;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            STUDENT[] students = new STUDENT[3];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"Введите фамилию и инициалы студента {i + 1}:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"Введите номер группы для студента {name}:");
+                int group = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Введите оценки (через пробел) для студента {name}:");
+                int[] marks = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                students[i] = new STUDENT(name, group, marks);
+            }
+
+            Array.Sort(students, (x, y) => x.fullName.CompareTo(y.fullName));
+
+            bool found = false;
+            foreach (var student in students)
+            {
+                if (Array.Exists(student.grades, grade => grade == 2))
+                {
+                    Console.WriteLine($"Студент, имеющий хотя бы одну двойку: {student.fullName}, Группа: {student.groupNumber}");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Студентов с оценкой 2 не найдено.");
+            }
+        }
+    }
+}
---
+++ Glava8/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Task2
+{
+    enum Position
+    {
+        Manager = 160,   
+        Developer = 180,    
+        QA = 150           
+    }
+
+    struct STUDENT
+    {
+        public string fullName;
+        public int groupNumber;
+        public int[] performance;
+
+        public STUDENT(string name, int group, int[] marks)
+        {
+            fullName = name;
+            groupNumber = group;
+            performance = marks;
+        }
+    }
+
+    class Accountant
+    {
+        public bool AskForBonus(Position worker, int hours)
+        {
+            int requiredHours = (int)worker;
+
+            if (hours > requiredHours)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position1 = Position.Manager;
+            Position position2 = Position.Developer;
+            Position position3 = Position.QA;
+
+            Accountant accountant = new Accountant();
+
+            Console.WriteLine("Менеджер получит премию: " + accountant.AskForBonus(Position.Manager, 170)); 
+            Console.WriteLine("Разработчик получит премию: " + accountant.AskForBonus(Position.Developer, 160)); 
+            Console.WriteLine("Тестировщик получит премию: " + accountant.AskForBonus(Position.QA, 160)); 
+        }
+    }
+}
