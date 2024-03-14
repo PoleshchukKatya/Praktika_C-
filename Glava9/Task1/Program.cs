3958d844ebaf8633ea01e17278f955e92f2cf7b8
Poleshchuk Katya <poleshchkat@gmail.com>
2024-03-14 12:53:49 +0300

    Добавляем Glava9

---
+++ Glava9/Task1/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+namespace Interface01
+{
+    interface Ix
+    {
+        void IxF0(int xKey); 
+        void IxF1();        
+    }
+    interface Iy
+    {
+        void F0(int xKey); 
+        void F1();        
+    }
+
+    interface Iz
+    {
+        void F0(int xKey);
+        void F1();       
+    }
+
+    interface Iw
+    {
+        void F0(int xKey); 
+        void F1();      
+    }
+
+    class TestClass : Ix, Iy, Iz, Iw
+    {
+        public int w; 
+
+        public TestClass()
+        {
+            w = 125; 
+        }
+
+        public TestClass(int key)
+        {
+            w = key; 
+        }
+
+        public void IxF0(int key)
+        {
+            w = key + 5;
+            Console.WriteLine("IxF0({0})...", w); 
+        }
+
+        public void IxF1()
+        {
+            w *= 5; 
+            Console.WriteLine("IxF1({0})...", w); 
+        }
+
+        public void F0(int xKey)
+        {
+            w = (int)Math.Pow(w, 3); 
+            Console.WriteLine("(Iy/Iz)F0({0})...", w); 
+        }
+        public void F1()
+        {
+            w = 7 * w - 2; 
+            Console.WriteLine("(Iy/Iz)F1({0})...", w); 
+        }
+
+        void Iw.F0(int xKey)
+        {
+            w = xKey + 5; 
+            Console.WriteLine("Iw.F0({0})...", w); 
+        }
+        void Iw.F1()
+        {
+            w -= 5; 
+            Console.WriteLine("Iw.F1({0})...", w); 
+        }
+
+        public void bF0()
+        {
+            Console.WriteLine("bF0()..."); 
+        }
+    }
+
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            TestClass x0 = new TestClass();
+            TestClass x1 = new TestClass(5);
+
+            x0.bF0(); 
+            x0.IxF0(10);
+            x1.IxF1();
+
+            x0.F0(5);
+            x1.F1();
+
+            (x0 as Iy).F0(7);
+            (x1 as Iz).F1();
+
+            Console.WriteLine("==========Prism test==========");
+            Console.WriteLine("==========Ix==========");
+            Ix ix = x1;
+            ix.IxF0(5);
+            ix.IxF1();
+
+            Console.WriteLine("==========Iy==========");
+            Iy iy = x1;
+            iy.F0(5);
+            iy.F1();
+
+            Console.WriteLine("==========Iz==========");
+            Iz iz = x1;
+            iz.F0(5);
+            iz.F1();
+
+            Console.WriteLine("==========Iw==========");
+            Iw iw = x1;
+            iw.F0(10);
+            iw.F1();
+        }
+    }
+}
---
+++ Glava9/Task2/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Linq;
+
+interface IComparable<T>
+{
+    int CompareTo(T other);
+}
+
+class Car : IComparable<Car>
+{
+    public string LicensePlate { get; set; }
+    public string Color { get; set; }
+    public string OwnerName { get; set; }
+    public bool IsPresent { get; set; }
+
+    public int CompareTo(Car other)
+    {
+        return LicensePlate.CompareTo(other.LicensePlate);
+    }
+}
+
+class ParkingLot
+{
+    private Car[] cars;
+
+    public ParkingLot(int capacity)
+    {
+        cars = new Car[capacity];
+    }
+
+    public void AddCar(Car car, int spot)
+    {
+        cars[spot] = car;
+    }
+
+    public Car FindCarByLicensePlate(string licensePlate)
+    {
+        foreach (Car car in cars)
+        {
+            if (car != null && car.LicensePlate == licensePlate)
+            {
+                return car;
+            }
+        }
+        return null;
+    }
+
+    public Car[] FindCarsByOwner(string ownerName)
+    {
+        return cars.Where(car => car != null && car.OwnerName == ownerName).ToArray();
+    }
+
+    public void PrintPresentCars()
+    {
+        Console.WriteLine("Присутствующие на стоянке автомобили:");
+        foreach (Car car in cars)
+        {
+            if (car != null && car.IsPresent)
+            {
+                Console.WriteLine($"Госномер: {car.LicensePlate}, Цвет: {car.Color}, Владелец: {car.OwnerName}");
+            }
+        }
+    }
+
+    public void PrintAbsentCars()
+    {
+        Console.WriteLine("Отсутствующие на стоянке автомобили:");
+        foreach (Car car in cars)
+        {
+            if (car != null && !car.IsPresent)
+            {
+                Console.WriteLine($"Госномер: {car.LicensePlate}, Цвет: {car.Color}, Владелец: {car.OwnerName}");
+            }
+        }
+    }
+
+    public Car GetCarInfoBySpot(int spot)
+    {
+        return cars[spot];
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ParkingLot parkingLot = new ParkingLot(10);
+
+        parkingLot.AddCar(new Car { LicensePlate = "A123BC", Color = "Red", OwnerName = "Иванов", IsPresent = true }, 0);
+        parkingLot.AddCar(new Car { LicensePlate = "B456DE", Color = "Blue", OwnerName = "Петров", IsPresent = true }, 1);
+        parkingLot.AddCar(new Car { LicensePlate = "C789FG", Color = "Green", OwnerName = "Сидоров", IsPresent = false }, 2);
+
+        while (true)
+        {
+            Console.WriteLine("\nВыберите действие:");
+            Console.WriteLine("1. Найти автомобиль по госномеру");
+            Console.WriteLine("2. Найти автомобили по владельцу");
+            Console.WriteLine("3. Вывести список присутствующих на стоянке автомобилей");
+            Console.WriteLine("4. Вывести список отсутствующих на стоянке автомобилей");
+            Console.WriteLine("5. Получить информацию по номеру места");
+            Console.WriteLine("6. Выйти");
+
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Пожалуйста, введите числовой вариант.");
+                continue;
+            }
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введите госномер для поиска:");
+                    string inputLicensePlate = Console.ReadLine();
+                    Car foundCar = parkingLot.FindCarByLicensePlate(inputLicensePlate);
+                    if (foundCar != null)
+                    {
+                        Console.WriteLine($"Найден автомобиль: Госномер: {foundCar.LicensePlate}, Цвет: {foundCar.Color}, Владелец: {foundCar.OwnerName}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Автомобиль с таким госномером не найден.");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Введите фамилию владельца для поиска:");
+                    string inputOwnerName = Console.ReadLine();
+                    Car[] carsByOwner = parkingLot.FindCarsByOwner(inputOwnerName);
+                    if (carsByOwner.Length > 0)
+                    {
+                        Console.WriteLine($"Найдены автомобили владельца {inputOwnerName}:");
+                        foreach (Car car in carsByOwner)
+                        {
+                            Console.WriteLine($"Госномер: {car.LicensePlate}, Цвет: {car.Color}, Владелец: {car.OwnerName}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Автомобили владельца {inputOwnerName} не найдены.");
+                    }
+                    break;
+                case 3:
+                    parkingLot.PrintPresentCars();
+                    break;
+                case 4:
+                    parkingLot.PrintAbsentCars();
+                    break;
+                case 5:
+                    Console.WriteLine("Введите номер места:");
+                    if (int.TryParse(Console.ReadLine(), out int spotNumber))
+                    {
+                        if (spotNumber >= 0 && spotNumber < 10)
+                        {
+                            Car carInfo = parkingLot.GetCarInfoBySpot(spotNumber);
+                            if (carInfo != null)
+                            {
+                                Console.WriteLine($"Информация по месту {spotNumber}: Госномер: {carInfo.LicensePlate}, Цвет: {carInfo.Color}, Владелец: {carInfo.OwnerName}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"На месте {spotNumber} нет автомобиля.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неправильный номер места. Допустимые значения от 0 до 9.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильный формат номера места.");
+                    }
+                    break;
+                case 6:
+                    Console.WriteLine("Программа завершена.");
+                    return;
+                default:
+                    Console.WriteLine("Неверный выбор. Пожалуйста, выберите действие из списка.");
+                    break;
+            }
+        }
+    }
+}
