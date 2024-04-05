af7ad6f8bf0981ff60526e1baddf16433fbecd6a
Poleshchuk Katya <poleshchkat@gmail.com>
2024-04-05 23:54:52 +0300

    Добавление Glava26

---
+++ Glava26/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using WpfApp1.models;
+using Microsoft.Extensions.Logging;
+using WpfApp1.Share;
+using System.Windows.Controls.Primitives;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private readonly XmlDocumentWorker _worker;
+        private string _xmlFilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _worker = new XmlDocumentWorker(new LoggerFactory().CreateLogger<XmlDocumentWorker>());
+            CheckButtonsAvailability();
+        }
+
+        private void PrintStudents(List<Student> students)
+        {
+            textBlockXmlFileContent.Text = "===Students===" + Environment.NewLine;
+            foreach (var student in students)
+            {
+                textBlockXmlFileContent.Text += student.ToString() + Environment.NewLine;
+            }
+        }
+
+        private void buttonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName;
+            dialog.DefaultExt = ".xml";
+            dialog.Filter = "Xml documents (.xml)|*.xml";
+
+            var result = dialog.ShowDialog();
+            if (result.HasValue && result.Value)
+            {
+                _xmlFilePath = dialog.FileName;
+                textBlockXMLPathFile.Text = _xmlFilePath;
+                _worker.Load(_xmlFilePath);
+                PrintStudents(_worker.GetAll());
+                CheckButtonsAvailability();
+            }
+        }
+
+        private void buttonFindStudentSurname_Click(object sender, RoutedEventArgs e)
+        {
+            var student = _worker.FindBy(textBoxStudentSurname.Text);
+            textBlockXmlFileContent.Text = student != null ? student.ToString() : "Student not found";
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var student = new Student
+            {
+                Surname = textBoxSurname.Text,
+                Name = textBoxName.Text,
+                BirthDate = DateTime.Parse(textBoxBirthDate.Text)
+            };
+
+            _worker.Add(student);
+            PrintStudents(_worker.GetAll());
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxDeleteSurname.Text))
+            {
+                _worker.Delete(textBoxDeleteSurname.Text);
+                PrintStudents(_worker.GetAll());
+            }
+        }
+
+        private void CheckButtonsAvailability()
+        {
+            bool isXmlFileSelected = !string.IsNullOrEmpty(_xmlFilePath);
+            buttonFindStudentSurname.IsEnabled = isXmlFileSelected;
+            buttonAdd.IsEnabled = isXmlFileSelected;
+            buttonDelete.IsEnabled = isXmlFileSelected;
+        }
+
+        private void buttonExit_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Glava26/WpfApp1/WpfApp1/interface/IXmlWorker.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using Microsoft.Extensions.Logging;
+
+namespace WpfApp1
+{
+    public interface IXmlWorker
+    {
+        void Load(string xmlDocPath);
+
+        void Add(Student student);
+
+        void Delete(string surname);
+
+        Student FindBy(string surname);
+
+        List<Student> GetAll();
+    }
+
+    public class Student
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Surname: {Surname}, Name: {Name}, BirthDate: {BirthDate.ToShortDateString()}";
+        }
+    }
+
+    public class XmlDocumentWorker : IXmlWorker
+    {
+        private readonly XmlDocument _document;
+        private readonly ILogger _logger;
+        private string _xmlFilePath;
+
+        public XmlDocumentWorker(ILogger logger)
+        {
+            _logger = logger;
+            _document = new XmlDocument();
+        }
+
+        public void Add(Student student)
+        {
+            var xRoot = _document.DocumentElement;
+
+            XmlElement studentElement = _document.CreateElement("студент");
+            studentElement.SetAttribute("фамилия", student.Surname);
+            studentElement.SetAttribute("имя", student.Name);
+            studentElement.SetAttribute("дата_рождения", student.BirthDate.ToString("yyyy-MM-dd"));
+
+            xRoot.AppendChild(studentElement);
+            _document.Save(_xmlFilePath);
+        }
+
+        public void Delete(string surname)
+        {
+            var xRoot = _document.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    var attributeSurname = xnode.Attributes.GetNamedItem("фамилия");
+                    try
+                    {
+                        var attributeSurnameText = attributeSurname?.InnerText;
+                        if (attributeSurnameText.Equals(surname))
+                        {
+                            xRoot.RemoveChild(xnode);
+                        }
+                    }
+                    catch (Exception ex) when (ex is XmlException || ex is NullReferenceException)
+                    {
+                        _logger.LogWarning(ex.Message, nameof(attributeSurname));
+                    }
+                }
+            }
+        }
+
+        public Student FindBy(string surname)
+        {
+            Student student = null;
+            var xRoot = _document.DocumentElement;
+            foreach (XmlNode xmlNode in xRoot)
+            {
+                student = GetStudent(xmlNode);
+                if (student.Surname.Equals(surname))
+                {
+                    return student;
+                }
+            }
+            return student;
+        }
+
+        public List<Student> GetAll()
+        {
+            List<Student> students = new List<Student>();
+            var xRoot = _document.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                var student = GetStudent(xnode);
+                students.Add(student);
+            }
+            return students;
+        }
+
+        public void Load(string xmlFilePath)
+        {
+            _xmlFilePath = xmlFilePath;
+            _document.Load(xmlFilePath);
+        }
+
+        public Student GetStudent(XmlNode node)
+        {
+            var student = new Student();
+            if (node.Attributes.Count > 0)
+            {
+                var attributeSurname = node.Attributes.GetNamedItem("фамилия");
+                var attributeName = node.Attributes.GetNamedItem("имя");
+                var attributeBirthDate = node.Attributes.GetNamedItem("дата_рождения");
+
+                student.Surname = attributeSurname?.Value;
+                student.Name = attributeName?.Value;
+                student.BirthDate = DateTime.ParseExact(attributeBirthDate?.Value, "yyyy-MM-dd", null);
+            }
+            return student;
+        }
+    }
+}
